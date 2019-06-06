ec3dd7a224dfd0dc6ca43e4a03553a8ee3d375ce
bjgribb <bjgribb@gmail.com>
2019-06-06 17:22:00 -0400

    created sample RSS dictionary and set current time for comparison

---
+++ RSS_activity.py
@@ -0,0 +1,15 @@
+import datetime
+
+current_time = datetime.datetime.utcnow()
+
+RSS_dict = {
+    'BBC': 'http://feeds.bbci.co.uk/news/rss.xml#',
+    'ESPN': 'https://www.espn.com/espn/rss/news',
+    'CNN': 'http://rss.cnn.com/rss/cnn_topstories.rss',
+    'Real Time with Bill Maher': 'http://billmaher.hbo.libsynpro.com/rss',
+    'Craigslist': 'https://www.craigslist.org/about/best/all/index.rss',
+    'CSS-Tricks': 'http://feeds.feedburner.com/CssTricks',
+    'Engadget': 'http://www.engadget.com/rss.xml',
+    'Reuters': 'http://feeds.reuters.com/reuters/technologyNews',
+    'Mashable': 'http://feeds.mashable.com/Mashable',
+}
