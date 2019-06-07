0bf048fb225dd96388850cbaa66992bc3ceaac9f
bjgribb <bjgribb@gmail.com>
2019-06-07 09:25:05 -0400

    created function to gather user input for number of days of inactivity and added in doc strings

--- RSS_activity.py
+++ RSS_activity.py
@@ -1,8 +1,6 @@
 import datetime
 import feedparser
 
-current_time = datetime.datetime.utcnow()
-
 RSS_dict = {
     'BBC': ['http://feeds.bbci.co.uk/news/rss.xml#'],
     'ESPN': ['https://www.espn.com/espn/rss/news', 'http://www.espn.com/espn/rss/nba/news'],
@@ -15,8 +13,18 @@ RSS_dict = {
     'Mashable': ['http://feeds.mashable.com/Mashable'],
 }
 
+def get_day_delta():
+    """Gathers user input for number of days of inactivity and converts to integer"""
+    try:
+        day_delta = int(input('Please enter the number of days of inactivity you would like to search for (i.e. 1, 2, 3) '))
+        return (day_delta * 24)
+    except ValueError:
+        print("Uh-oh that wasn't a number please enter a whole number.")
+        return get_day_delta()
+
 
 def get_RSS_time(rss_url):
+    """Utilizes feedparser to gather published date of latest entry"""
     try:
         RSS_time = feedparser.parse(rss_url).entries[0].published_parsed
         return RSS_time
@@ -26,7 +34,11 @@ def get_RSS_time(rss_url):
 
 
 def get_inactive_RSS_feeds(RSS_dict):
+    """Iterates thru RSS url feeds and compares published time to current time to
+    to determine which companies have not been updated in a given number of days"""
     inactive_RSS_companies = []
+    current_time = datetime.datetime.utcnow()
+    time_delta = datetime.timedelta(hours=get_day_delta())
     for k in RSS_dict:
         for v in RSS_dict[k]:
             latest_RSS_time = get_RSS_time(v)
@@ -39,11 +51,9 @@ def get_inactive_RSS_feeds(RSS_dict):
                 latest_RSS_time.tm_sec
             )
             print(k, latest_RSS_datetime)
-            time_delta = datetime.timedelta(hours=24)
             if current_time - latest_RSS_datetime > time_delta:
                 inactive_RSS_companies.append(k)
     return inactive_RSS_companies
 
 
 print(get_inactive_RSS_feeds(RSS_dict))
-print('current time', current_time)
